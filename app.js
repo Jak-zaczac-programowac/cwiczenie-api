d0860c9b41e733e2feb488a85d26aaae918b7776
Kacper Sokołowski <kontakt@kaapa.pl>
2021-06-08 20:03:48 +0200

    Kontynuacja zadania. Klon insta

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
-const getImagesButton = document.querySelector("#get-images");
 const firstScreen = document.querySelector("#first-screen");
 const imagesGrid = document.querySelector("#images-grid");
 const amountofImages = 9;
 
-let images;
+let currentPage = 1;
+let loading = false;
 
-getImagesButton.addEventListener("click", async () => {
-  if (!images) {
-    images = await fetch(
-      `https://picsum.photos/v2/list?limit=${amountofImages}`
-    ).then((res) => res.json());
+function getImageUrls(page = 1) {
+  return fetch(
+    `https://picsum.photos/v2/list?page=${page}&limit=${amountofImages}`
+  ).then((res) => res.json());
+}
 
-    const imageElements = images.map((image) => {
-      const img = document.createElement("img");
-      img.src = image.download_url;
-      return img;
-    });
+function renderImages(container, images) {
+  const imageElements = images.map((image) => {
+    const img = document.createElement("img");
+    img.src = image.download_url;
+    return img;
+  });
 
-    imageElements.forEach((img) => {
-      imagesGrid.appendChild(img);
-    });
+  imageElements.forEach((img) => {
+    container.appendChild(img);
+  });
+}
 
-    imagesGrid.hidden = false;
-    firstScreen.hidden = true;
+async function getAndRenderImages(page = 1) {
+  const images = await getImageUrls(page);
+  renderImages(imagesGrid, images);
+}
 
-    console.log(imageElements);
+getAndRenderImages(1);
+
+document.addEventListener("scroll", (e) => {
+  if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    if (!loading) {
+      loading = true;
+      currentPage = currentPage + 1;
+      getAndRenderImages(currentPage).then(() => {
+        loading = false;
+      });
+    }
   }
 });
